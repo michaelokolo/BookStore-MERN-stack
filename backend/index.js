88aafa0d6ff688209aede987512eb3ff03f5892b
Michael Okolo <michaelokolo62@yahoo.com>
2024-01-12 20:31:13 -0500

    completed the book model

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { PORT } from './config.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import { Book } from './models/bookModel.js';
 
 dotenv.config();
 
@@ -10,6 +11,31 @@ const app = express();
 app.get('/', (request, response) => {
   console.log(request);
   return response.status(234).send('Welcome To MERN Stack Tutorial');
+});
+
+// Route for Save a new book
+app.post('/books', async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: 'Send all required fields: title, author, publishYear',
+      });
+    }
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
+    const book = await Book.create(newBook);
+    return response.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
 });
 
 mongoose
