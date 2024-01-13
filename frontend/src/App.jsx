274df315df7cb15b6fe0be500ef393b16c95ecf5
Michael Okolo <michaelokolo62@yahoo.com>
2024-01-13 18:23:33 -0500

    setting up frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+const App = () => {
+  return <div className="bg-red-400 text-white">App</div>;
+};
+
+export default App;
